74d6461e1a84b813e4da18c53b9afcbed2af7e86
Kevin Harrington <kevintharrington@gmail.com>
2013-06-20 13:50:26 -0700

    Taking into account pre-multiplied alpha in the second image for GPUImageAlphaBlendFilter. Fixes: text aliasing issues with GPUImageAlphaBlendFilter. See post: http://stackoverflow.com/questions/15934381/aliasing-issue-with-gpuimageview

--- framework/Source/GPUImageAlphaBlendFilter.m
+++ framework/Source/GPUImageAlphaBlendFilter.m
@@ -4,20 +4,25 @@ NSString *const kGPUImageAlphaBlendFragmentShaderString = SHADER_STRING
 (
  varying highp vec2 textureCoordinate;
  varying highp vec2 textureCoordinate2;
-
+ 
  uniform sampler2D inputImageTexture;
  uniform sampler2D inputImageTexture2;
  
  uniform lowp float mixturePercent;
-
+ 
  void main()
  {
 	 lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
 	 lowp vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate2);
 	 
-	 gl_FragColor = vec4(mix(textureColor.rgb, textureColor2.rgb, textureColor2.a * mixturePercent), textureColor.a);
+	 if (textureColor2.a == 0.0) {
+		 gl_FragColor = textureColor;
+	 } else {
+		 gl_FragColor = vec4(mix(textureColor.rgb, textureColor2.rgb / textureColor2.a,
+								 mixturePercent * textureColor2.a), textureColor.a);
+	 }
  }
-);
+ );
 
 
 @implementation GPUImageAlphaBlendFilter
